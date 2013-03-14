43ebc387900b0023197f6ef785b2f56cef50d6f2
michael eschbacher <michaeleschbacher@gmail.com>
2013-03-13 22:48:05 -0500

    Added a whole bunch of tests for the set orderby

--- tests/FetcherTest.php
+++ tests/FetcherTest.php
@@ -4,10 +4,64 @@ require 'bootstrap.php';
 
 class FetcherTest extends PHPUnit_Framework_TestCase
 {
-
-    public function testFailure()
+    public function testSetOrderBySingleColumn()
     {
-        $this->fail('just looking');
+        $fetcher = new Mock_Fetcher();
+        $fetcher->setOrderBy('id');
+        $this->assertEquals('id', $fetcher->orderBy);
     }
+    public function testSetOrderBySingleColumnWithDirection()
+    {
+        $expected = array(
+            'id ASC' => 'id ASC',
+            'name desc' => 'name DESC'
+        );
+        
+        $fetcher = new Mock_Fetcher();
+        foreach ($expected as $input=>$output) {
+            $fetcher->setOrderBy($input);
+            $this->assertEquals($output, $fetcher->orderBy);
+        }
+    }
+    
+    public function testSetOrderByMultipleColumns()
+    {
+        $expected = array(
+            'id, title' => 'id, title',
+            'id desc, title Asc' => 'id DESC, title ASC'
+        );
+        
+        $fetcher = new Mock_Fetcher();
+        foreach ($expected as $input => $output) {
+            $fetcher->setOrderBy($input);
+            $this->assertEquals($output, $fetcher->orderBy);
+        }
+    }
+    
+    public function testSetOrderByBadParameters()
+    {
+        $inputs = array(
+            'id asd',
+            '',
+            'id ASC, review taco'
+        );
+        
+        $fetcher = new Mock_Fetcher();
+        foreach ($inputs as $input) {
+            $fetcher->setOrderBy($input);
+            $this->assertNull($fetcher->orderBy);
+        }
+    }
+}
 
+class Mock_Fetcher extends Fetcher
+{
+    public function __get($param)
+            {
+        return $this->$param;
+            }
+            public function __set($param, $val)
+                {
+                $this->$param = $val;
+                }
 }
